9354c34f2ab1556b7a70ecebf252bf8743dffda5
Ilham Muhammad <ilhamhd98@gmail.com>
2021-11-26 06:57:16 +0700

    initial commit

---
+++ tests/CounterTest.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace ilhamhmmd\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class CounterTest extends TestCase
+{
+    public function testCounter()
+    {
+        $counter = new Counter();
+        $counter->increment();
+
+        echo $counter->getCounter() . PHP_EOL;
+    }
+}
