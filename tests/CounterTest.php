14e32ffdbe45b87ca5f699d9961bf516f57e7e78
Ilham Muhammad <ilhamhd98@gmail.com>
2021-11-30 06:57:08 +0700

    add test

--- tests/CounterTest.php
+++ tests/CounterTest.php
@@ -1,16 +1,28 @@
-<?php 
+<?php
 
 namespace ilhamhmmd\Test;
 
+use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\TestCase;
 
 class CounterTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function increment()
+    {
+        $counter = new Counter();
+        
+        $counter->increment();
+        $this->assertEquals(1, $counter->getCounter());
+    }
+
     public function testCounter()
     {
         $counter = new Counter();
+        
         $counter->increment();
-
-        echo $counter->getCounter() . PHP_EOL;
+        $this->assertEquals(1, $counter->getCounter());
     }
 }
